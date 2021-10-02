0591add3b608c30feb00404d47016c641121d3dd
mpgxc <mpgx5.c@gmail.com>
2021-10-02 14:24:38 -0300

    chore: added setup jest and added test on commit pipeline

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  bail: true,
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+};
