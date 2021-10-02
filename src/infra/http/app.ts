e81ba6b14fb23acf9df09be377c05ab216e5edf1
mpgxc <mpgx5.c@gmail.com>
2021-10-02 16:34:53 -0300

    feat: added routes endpoints to http app

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,13 +1,17 @@
+import 'reflect-metadata';
 import 'express-async-errors';
+import '@shared/container';
 
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { routes } from 'presenter/routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
+app.use('/api', routes);
 
 export { app };
