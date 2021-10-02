7ec2682b710e2ff03892517300a7ee595b3bddb1
mpgxc <mpgx5.c@gmail.com>
2021-10-02 13:17:59 -0300

    feat: added express adapter routers

---
+++ src/infra/http/adapters/ExpressAdapter.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+
+import { HttpRequest } from '@shared/http/HttpRequest';
+
+type ControllerReponse = {
+  (request: HttpRequest);
+};
+
+class ExpressAdapter {
+  static apply(CtxController: ControllerReponse) {
+    return async (request: Request, response: Response): Promise<Response> => {
+      const httpResponse = await CtxController(request);
+      /**
+       * Success status code range: 200 - 299
+       * Error/Others: < 200 or > 299
+       */
+      if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+        return response.status(httpResponse.statusCode).json(httpResponse.body);
+      }
+
+      return response
+        .status(httpResponse.statusCode)
+        .json({ ...httpResponse.body });
+    };
+  }
+}
+
+export { ExpressAdapter };
