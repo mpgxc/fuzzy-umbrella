0302600c486c9209086b6a48f9807ab0ce4951d2
mpgxc <mpgx5.c@gmail.com>
2021-10-04 03:49:25 -0300

    feat: added invalid gender error

---
+++ src/application/customers/errors/InvalidGenreError.ts
@@ -0,0 +1,11 @@
+import { BaseAppException } from '@shared/excpetions';
+
+class InvalidGenreError extends BaseAppException {
+  constructor(value: string) {
+    super();
+    this.message = `The Genre <${value}> does not exists! Only accepted <MALE and FEMALE>`;
+    this.name = 'InvalidGenreError';
+  }
+}
+
+export { InvalidGenreError };
