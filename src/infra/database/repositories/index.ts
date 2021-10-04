8bd46b619874e4cc94bd1dd4a34a9f164e9a3b79
mpgxc <mpgx5.c@gmail.com>
2021-10-04 03:24:05 -0300

    feat: export repos

--- src/infra/database/repositories/index.ts
+++ src/infra/database/repositories/index.ts
@@ -1,2 +1,4 @@
 export { CustomersRepository as MemoryCustomersRepository } from './memory/CustomersRepository';
 export { CityRepository as MemoryCityRepository } from './memory/CityRepository';
+export { CustomerRepository } from './prisma/CustomerRepository';
+export { CityRepository } from './prisma/CityRepository';
