e243b37c3eddac25c1560f82471c808ecea57b28
mpgxc <mpgx5.c@gmail.com>
2021-10-04 03:47:01 -0300

    test: added new checking for validate gender

--- src/presenter/controllers/__tests__/RegisterCustomerController.spec.ts
+++ src/presenter/controllers/__tests__/RegisterCustomerController.spec.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@infra/database/prisma';
 import { app } from '@infra/http/app';
 import request from 'supertest';
+import { v4 } from 'uuid';
 
 describe('RegisterCustomerController', () => {
   afterAll(async () => {
@@ -31,5 +32,49 @@ describe('RegisterCustomerController', () => {
 
     expect(response.body).toEqual('');
     expect(response.status).toBe(201);
+    expect(
+      await prisma.customer.findFirst({
+        where: {
+          full_name: 'Mateus Pinto Garcia',
+        },
+      }),
+    ).toBeTruthy();
+  });
+
+  it('should not be to register new customers with wrong city_id!', async () => {
+    const response = await request(app)
+      .post('/api/customers')
+      .send({
+        birth_date: new Date('10-14-1995'),
+        full_name: 'Mateus Pinto Garcia',
+        genre: 'MALE',
+        city_id: v4(),
+      });
+
+    expect(response.body.status).toEqual('Exception!');
+    expect(response.body.name).toEqual('CityNotFoundError');
+    expect(response.status).toBe(404);
+  });
+
+  it('should not be to register new customers with non-existent gender!', async () => {
+    const city = await prisma.city.create({
+      data: {
+        country: 'Piauí',
+        name: 'Esperantina',
+      },
+    });
+
+    const response = await request(app)
+      .post('/api/customers')
+      .send({
+        birth_date: new Date('10-14-1995'),
+        full_name: 'Luzia Pinto Garcia',
+        genre: 'MALE_FEMALE',
+        city_id: city.id,
+      });
+
+    expect(response.body.status).toEqual('Exception!');
+    expect(response.body.name).toEqual('InvalidGenreError');
+    expect(response.status).toBe(400);
   });
 });
