084ae2789b3388c2762c9d043b6650d2b8d24d87
mpgxc <mpgx5.c@gmail.com>
2021-10-08 09:17:36 -0300

    test: added integration test of remove customer usecase

---
+++ src/presenter/controllers/__tests__/RemoveCustomerController.spec.ts
@@ -0,0 +1,66 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { prisma } from '@infra/database/prisma';
+import { app } from '@infra/http/app';
+import faker from 'faker';
+import request from 'supertest';
+import { v4 } from 'uuid';
+
+import { City } from '.prisma/client';
+
+let city: City;
+
+describe('RemoveCustomerController', () => {
+  beforeAll(async () => {
+    city = await prisma.city.create({
+      data: {
+        country: 'Piauí',
+        name: 'São João do Arraial',
+      },
+    });
+  });
+
+  afterAll(async () => {
+    const deleteAllCustomers = prisma.customer.deleteMany();
+    const deleteAllCities = prisma.city.deleteMany();
+
+    await prisma.$transaction([deleteAllCustomers, deleteAllCities]);
+
+    await prisma.$disconnect();
+  });
+
+  it('should be able to delete specific customer by id!', async () => {
+    const customer = await prisma.customer.create({
+      data: {
+        birth_date: faker.date.past(),
+        full_name: faker.name.findName(),
+        genre: faker.random.arrayElement(['MALE', 'FEMALE']),
+        city_id: city.id,
+      },
+    });
+
+    /**
+     * Test Case
+     */
+
+    const response = await request(app).delete(`/api/customers/${customer.id}`);
+
+    expect(response.status).toBe(204);
+    expect(response.body).toEqual({});
+
+    const customerExists = await prisma.customer.findUnique({
+      where: {
+        id: customer.id,
+      },
+    });
+    expect(customerExists).toBe(null);
+    expect(customerExists).toBeFalsy();
+  });
+
+  it('should not be able to delete specific customer by wrong/invalid id!', async () => {
+    const response = await request(app).delete(`/api/customers/${v4()}`);
+
+    expect(response.body.status).toEqual('Exception!');
+    expect(response.body.name).toEqual('CustomerNotFoundError');
+    expect(response.status).toBe(404);
+  });
+});
