8a3f8916ff64a4a0b61c3aa83464e9ed6b06dd77
mpgxc <mpgx5.c@gmail.com>
2021-10-08 16:48:31 -0300

    fix: fixing paths

--- src/presenter/factories/CustomerControllers.ts
+++ src/presenter/factories/CustomerControllers.ts
@@ -1,8 +1,8 @@
-import { ListCustomerController } from '@presenter/controllers/ListCustomerController';
-import { RegisterCustomerController } from '@presenter/controllers/RegisterCustomerController';
-import { RemoveCustomerController } from '@presenter/controllers/RemoveCustomerController';
-import { ShowCustomerController } from '@presenter/controllers/ShowCustomerController';
-import { UpdateCustomerController } from '@presenter/controllers/UpdateCustomerController';
+import { ListCustomerController } from '@presenter/controllers/customers/ListCustomerController';
+import { RegisterCustomerController } from '@presenter/controllers/customers/RegisterCustomerController';
+import { RemoveCustomerController } from '@presenter/controllers/customers/RemoveCustomerController';
+import { ShowCustomerController } from '@presenter/controllers/customers/ShowCustomerController';
+import { UpdateCustomerController } from '@presenter/controllers/customers/UpdateCustomerController';
 
 const registerCustomerController = new RegisterCustomerController();
 const listCustomerController = new ListCustomerController();
