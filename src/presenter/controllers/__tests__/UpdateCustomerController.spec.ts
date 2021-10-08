24013196d1e22130f751b27311c1d10999d08b4f
mpgxc <mpgx5.c@gmail.com>
2021-10-08 11:43:24 -0300

    test: added update customer integration test

---
+++ src/presenter/controllers/__tests__/UpdateCustomerController.spec.ts
@@ -0,0 +1,121 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { prisma } from '@infra/database/prisma';
+import { app } from '@infra/http/app';
+import faker from 'faker';
+import request from 'supertest';
+import { v4 } from 'uuid';
+
+import { City } from '.prisma/client';
+
+let city: City;
+
+describe('UpdateCustomerController - PATCH customer/:id', () => {
+  beforeAll(async () => {
+    city = await prisma.city.create({
+      data: {
+        country: 'Bahia',
+        name: 'Salvador',
+      },
+    });
+  });
+
+  afterAll(async () => {
+    const deleteAllCustomers = prisma.customer.deleteMany();
+    const deleteAllCities = prisma.city.deleteMany();
+
+    await prisma.$transaction([deleteAllCustomers, deleteAllCities]);
+
+    await prisma.$disconnect();
+  });
+
+  it('should be able to update specific customer name by id!', async () => {
+    const customer = await prisma.customer.create({
+      data: {
+        birth_date: faker.date.past(),
+        full_name: faker.name.findName(),
+        genre: faker.random.arrayElement(['MALE', 'FEMALE']),
+        city_id: city.id,
+      },
+    });
+
+    /**
+     * Test Case
+     */
+
+    const response = await request(app)
+      .patch(`/api/customers/${customer.id}`)
+      .send({
+        full_name: 'João das Neves',
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual('');
+
+    const customerUpdated = await prisma.customer.findUnique({
+      where: {
+        id: customer.id,
+      },
+    });
+
+    expect(customerUpdated.full_name).toEqual('João das Neves');
+  });
+
+  it('should not be able to update specific customer name by wrong/invalid id!', async () => {
+    const response = await request(app).get(`/api/customers/${v4()}`).send({
+      full_name: 'Random Name',
+    });
+
+    expect(response.body.status).toEqual('Exception!');
+    expect(response.body.name).toEqual('CustomerNotFoundError');
+    expect(response.status).toBe(404);
+  });
+
+  it('should not be able to update specific customer name with wrong/invalid params!', async () => {
+    const customer = await prisma.customer.create({
+      data: {
+        birth_date: faker.date.past(),
+        full_name: faker.name.findName(),
+        genre: faker.random.arrayElement(['MALE', 'FEMALE']),
+        city_id: city.id,
+      },
+    });
+
+    const response = await request(app)
+      .patch(`/api/customers/${customer.id}`)
+      .send({
+        full_name: '',
+      });
+
+    expect(response.status).toBe(422);
+    expect(response.body).toBeInstanceOf(Array);
+
+    const [first] = response.body;
+
+    expect(first.status).toEqual('Exception!');
+    expect(first.type).toEqual('string.empty');
+  });
+
+  it('should not be able to update specific customer name with body null!', async () => {
+    const customer = await prisma.customer.create({
+      data: {
+        birth_date: faker.date.past(),
+        full_name: faker.name.findName(),
+        genre: faker.random.arrayElement(['MALE', 'FEMALE']),
+        city_id: city.id,
+      },
+    });
+
+    const response = await request(app)
+      .patch(`/api/customers/${customer.id}`)
+      .send({});
+
+    expect(response.status).toBe(422);
+    expect(response.body).toBeInstanceOf(Array);
+
+    const [first] = response.body;
+
+    expect(first.status).toEqual('Exception!');
+    expect(first.type).toEqual('any.required');
+    expect(first.label).toEqual('full_name');
+  });
+});
