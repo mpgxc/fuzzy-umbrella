9357ff8f0793b71da00c44ad89634e33e331f738
mpgxc <mpgx5.c@gmail.com>
2021-10-09 06:28:14 -0300

    test: added integration test to show cities by id

---
+++ src/presenter/controllers/cities/__tests__/ShowCityController.spec.ts
@@ -0,0 +1,52 @@
+import { prisma } from '@infra/database/prisma';
+import { app } from '@infra/http/app';
+import request from 'supertest';
+import { v4 } from 'uuid';
+
+describe('ShowCityController - GET cities/:id', () => {
+  afterAll(async () => {
+    const deleteAllCities = prisma.city.deleteMany();
+
+    await prisma.$transaction([deleteAllCities]);
+
+    await prisma.$disconnect();
+  });
+
+  it('should be able to find cities by id!', async () => {
+    const city = await prisma.city.create({
+      data: {
+        country: 'SP',
+        name: 'São Paulo',
+      },
+    });
+
+    const response = await request(app).get(`/api/cities/${city.id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeTruthy();
+
+    expect(response.body.id).toBe(city.id);
+    expect(response.body.name).toBe(city.name);
+    expect(response.body.country).toBe(city.country);
+  });
+
+  it('should not be able to find cities by wrong/invalid id!', async () => {
+    const response = await request(app).get(`/api/cities/${v4()}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.status).toEqual('Exception!');
+    expect(response.body.name).toEqual('CityNotFoundError');
+  });
+
+  it('should not be able to find cities by invalid format id!', async () => {
+    const response = await request(app).get('/api/cities/sasasasasas');
+
+    expect(response.status).toBe(422);
+
+    const [first] = response.body;
+
+    expect(first.status).toEqual('Exception!');
+    expect(first.type).toEqual('string.guid');
+    expect(first.label).toEqual('id');
+  });
+});
