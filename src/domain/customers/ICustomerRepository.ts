5869872edc8807ea1258925d96b06fc48c5007a8
mpgxc <mpgx5.c@gmail.com>
2021-10-13 19:26:33 -0300

    refactor: remove common methods on custom repositories

--- src/domain/customers/ICustomerRepository.ts
+++ src/domain/customers/ICustomerRepository.ts
@@ -1,17 +1,13 @@
+import { IBaseRepository } from '@domain/common/IBaseRepository';
+
 import { Customer } from './Customer';
 import { RenderCustomerResponse } from './CustomerMapper';
 
 type CustomerRender = Partial<RenderCustomerResponse>;
 
-interface ICustomerRepository {
-  create(customer: Customer, city_id: string): Promise<void>;
-  save(customer: Customer): Promise<void>;
-  delete(id: string): Promise<void>;
-
-  findById(id: string): Promise<Customer>;
-  findByIdRender(id: string): Promise<CustomerRender>;
+interface ICustomerRepository
+  extends IBaseRepository<Customer, CustomerRender> {
   listByName(name: string): Promise<CustomerRender[]>;
-  list(name?: string): Promise<CustomerRender[]>;
 }
 
 export { ICustomerRepository, CustomerRender };
